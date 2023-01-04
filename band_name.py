a4b0652213da3fdcd324e7f623496c020520020c
Curtis Olinger <curtisolinger@gmail.com>
2023-01-04 10:42:53 -0800

    Initial commit

---
+++ band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator program!")
+city = input("What city did you grow up in?\n")
+pet = input("Please enter the name of your favorite pet?\n")
+print(f"Your band name is {city + ' ' + pet}")
